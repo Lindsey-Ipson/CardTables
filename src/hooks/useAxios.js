edcf10c78a3285e63dd548f29387b08a722bce0a
Lindsey Ipson <lindseyipson@Lindseys-MacBook-Air.local>
2023-12-03 19:39:50 -0800

    connect useAxios to PokeDex component

--- src/hooks/useAxios.js
+++ src/hooks/useAxios.js
@@ -5,10 +5,24 @@ function useAxios ( url ) {
   const [data, setData] = useState([]);
   const [error, setError] = useState(null);
 
-  const addData = async () => {
+  const addData = async (restOfUrl = false) => {
     try {
-      const response = await axios.get(url);
-      setData(data => [...data, response.data]);
+
+
+      console.log('url', url)
+      console.log('restOfUrl', restOfUrl)
+
+      let response;
+      if (restOfUrl) {
+        console.log('passed restOfUrl')
+        response = await axios.get(`${url}${restOfUrl}`);
+      }
+      else if (restOfUrl === false) {
+        console.log('passed false')
+        response = await axios.get(url);
+      }
+      
+      setData(prevData => [...prevData, response.data]);
     } catch (error) {
       setError(error);
     }
@@ -20,5 +34,43 @@ function useAxios ( url ) {
 
   return [data, addData, clearData, error];
 }
+// function useAxios ( url ) {
+//   const [data, setData] = useState([]);
+//   const [error, setError] = useState(null);
+
+//   const addData = async (restOfUrl = '') => {
+//     try {
+//       const response = await axios.get(`${url}${restOfUrl}`);
+//       setData(prevData => [...prevData, response.data]);
+//     } catch (error) {
+//       setError(error);
+//     }
+//   };
+
+//   const clearData = () => {
+//     setData([]);
+//   };
+
+//   return [data, addData, clearData, error];
+// }
+// function useAxios ( url ) {
+//   const [data, setData] = useState([]);
+//   const [error, setError] = useState(null);
+
+//   const addData = async () => {
+//     try {
+//       const response = await axios.get(url);
+//       setData(data => [...data, response.data]);
+//     } catch (error) {
+//       setError(error);
+//     }
+//   };
+
+//   const clearData = () => {
+//     setData([]);
+//   };
+
+//   return [data, addData, clearData, error];
+// }
 
 export default useAxios;
