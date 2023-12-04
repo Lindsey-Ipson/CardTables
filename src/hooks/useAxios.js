2436ec422cc3de930af96756996b8c98fda13ae7
Lindsey Ipson <lindseyipson@Lindseys-MacBook-Air.local>
2023-12-03 20:05:42 -0800

    clean up code

--- src/hooks/useAxios.js
+++ src/hooks/useAxios.js
@@ -5,22 +5,9 @@ function useAxios ( url ) {
   const [data, setData] = useState([]);
   const [error, setError] = useState(null);
 
-  const addData = async (restOfUrl = false) => {
+  const addData = async (restOfUrl='') => {
     try {
-
-
-      console.log('url', url)
-      console.log('restOfUrl', restOfUrl)
-
-      let response;
-      if (restOfUrl) {
-        console.log('passed restOfUrl')
-        response = await axios.get(`${url}${restOfUrl}`);
-      }
-      else if (restOfUrl === false) {
-        console.log('passed false')
-        response = await axios.get(url);
-      }
+      const response = await axios.get(`${url}${restOfUrl}`);
       
       setData(prevData => [...prevData, response.data]);
     } catch (error) {
@@ -34,43 +21,5 @@ function useAxios ( url ) {
 
   return [data, addData, clearData, error];
 }
-// function useAxios ( url ) {
-//   const [data, setData] = useState([]);
-//   const [error, setError] = useState(null);
-
-//   const addData = async (restOfUrl = '') => {
-//     try {
-//       const response = await axios.get(`${url}${restOfUrl}`);
-//       setData(prevData => [...prevData, response.data]);
-//     } catch (error) {
-//       setError(error);
-//     }
-//   };
-
-//   const clearData = () => {
-//     setData([]);
-//   };
-
-//   return [data, addData, clearData, error];
-// }
-// function useAxios ( url ) {
-//   const [data, setData] = useState([]);
-//   const [error, setError] = useState(null);
-
-//   const addData = async () => {
-//     try {
-//       const response = await axios.get(url);
-//       setData(data => [...data, response.data]);
-//     } catch (error) {
-//       setError(error);
-//     }
-//   };
-
-//   const clearData = () => {
-//     setData([]);
-//   };
-
-//   return [data, addData, clearData, error];
-// }
 
 export default useAxios;
